8a06023f41b180eb4286e983e1b7dea7221e5156
dofirtwo <santiago19267@gmail.com>
2023-04-24 11:23:38 -0500

    segundo commit

---
+++ views/roles.frm.php
@@ -0,0 +1,9 @@
+<?php include_once "header.php"; ?>    
+<div class="row">
+    <div class="col-12">
+        <!-- //Formulario -->
+        <h1 class="text-center">Formulario Roles</h1>
+        <!-- //Fin Formulario -->
+    </div>
+</div>
+<?php include_once "footer.php";?>
